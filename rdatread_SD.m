4618fbbab368d5142f759a999372db4f5debbb66
Krista Perks <kperky@gmail.com>
2013-07-16 16:41:47 -0700

    added cue to trial structure

--- rdatread_SD.m
+++ rdatread_SD.m
@@ -2,7 +2,7 @@ function Trial = rdatread_SD(fname)
 % Script to read in cleaned up rdat file
 % set up for go-nogo
 fid=fopen(fname);
-C=textscan(fid, '%d %d %d %s %s %d %d %d %d %d %d %d %d', ...
+C=textscan(fid, '%d %d %d %s %s %d %d %d %d %d %d %d %d %d', ...
     'CommentStyle', {'File', 'Date'});
 fclose(fid);
 
@@ -13,12 +13,13 @@ Trial.trialno=C{2};
 Trial.trialtype=C{3};
 Trial.anchor=C{4};
 Trial.target=C{5};
-Trial.REPcnt=C{6};
+Trial.rep=C{6};
 Trial.max=C{7};
 Trial.targetplay=C{8};
-Trial.RspAc=C{9};
+Trial.resp=C{9};
 Trial.reinfor=C{10};
-Trial.rxntime=C{11};
-Trial.TOD=C{12};
-Trial.day=C{13};
+Trial.cue=C{11};
+Trial.rxntime=C{12};
+Trial.tod=C{13};
+Trial.day=C{14};
 
